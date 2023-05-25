34437cff9ad161196b177b5708a78d0045d642ec
JohanaMontenegro <yoh.teencontre@gmail.com>
2023-05-24 22:01:39 -0300

    cambios de alineacion carrousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,23 @@
 <template>
-    <div id="home" class="row justify-content-center">
-        <div id="main-home" class="mx-4 col-10 rounded border border-primary">
-            <div id="homeContent">
+    <div class="row">
+        <div id="main-home" class="justify-content-center ">
+            <div id="homeContent" class="col-11 rounded border border-primary">
                 <div id="carousel-captions" class="carousel slide">
-                    <div class="carousel-indicators mb-0">
-                        <button type="button" data-bs-target="#carousel-captions" data-bs-slide-to="0" class="active mt-3"
-                            aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carousel-captions" data-bs-slide-to="1" class="mt-3"
-                            aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carousel-captions" data-bs-slide-to="2" class="mt-3"
-                            aria-label="Slide 3"></button>
-                        <button type="button" data-bs-target="#carousel-captions" data-bs-slide-to="3" class="mt-3"
-                            aria-label="Slide 4"></button>
-                        <button type="button" data-bs-target="#carousel-captions" data-bs-slide-to="4" class="mt-3"
-                            aria-label="Slide 5"></button>
-                    </div>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img src="./img/AllinOneDell.png" class="d-block w-50 h-75 m-auto" alt="...">
                             <div class="carousel-captions d-none d-md-block text-center">
                                 <h5><strong> Pc All in One Dell</strong></h5>
-                                <p>Pc de escritorio All-in-One AIO De 21,5 pulgadas Dell Optiplex 3280 Windows 11 Home Intel Core i5 / 4 GB RAM / 1 TB </p>
+                                <p>Pc de escritorio All-in-One AIO De 21,5 pulgadas Dell Optiplex 3280 Windows 11 Home Intel
+                                    Core i5 / 4 GB RAM / 1 TB </p>
                             </div>
                         </div>
                         <div class="carousel-item">
                             <img src="./img/computadora-Game.png.png" class="d-block w-50 h-75 m-auto" alt="...">
                             <div class="carousel-captions d-none d-md-block text-center">
                                 <h5> <strong>Pc Gamer</strong></h5>
-                                <p>Computadora Gamer IbuYPower 7 5700G 16GB SSD 480. Incluye WIFI, monitor 22", teclado y mouse retroiluminados</p>
+                                <p>Computadora Gamer IbuYPower 7 5700G 16GB SSD 480. Incluye WIFI, monitor 22", teclado y
+                                    mouse retroiluminados</p>
                             </div>
                         </div>
                         <div class="carousel-item">
@@ -41,7 +31,9 @@
                             <img src="./img/gabinete.png" class="d-block w-50 h-75 m-auto" alt="...">
                             <div class="carousel-captions d-none d-md-block text-center">
                                 <h5><strong>Gabinete Gamer Aerox Hebe ARX 340G 3X FAN 120MM </strong></h5>
-                                <p>Es un gabinete Gamer con gran espacio, Ventana lateral transparente, Luces RGB en el frontal, USB tanto 2.0 como 3.0 y numerosas ranuras de expansión para seguir ampliando tu setup.</p>
+                                <p>Es un gabinete Gamer con gran espacio, Ventana lateral transparente, Luces RGB en el
+                                    frontal, USB tanto 2.0 como 3.0 y numerosas ranuras de expansión para seguir ampliando
+                                    tu setup.</p>
                             </div>
                         </div>
                         <div class="carousel-item">
@@ -76,11 +68,11 @@ export default {
 </script>
 
 <style scoped>
-#home.row {
-    margin-left: 180px;
+.row {
+    display: flex;
+    margin-left: 260px;
 }
 
-#main-home {
+#homeContent {
     background-color: rgb(198, 213, 255);
-}
-</style>
+}</style>
--- src/views/ProductosView.vue
+++ src/views/ProductosView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="content">
+    <div id="content" class="d-flex flex-row col-9 ">
     <div>
         <h1 class="animate__animated animate__zoomIn"> Productos </h1>
     </div>
