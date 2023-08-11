18ba8aca06d09136be5cfe007bebeb847d46e385
AidaHerreravilar <aidaherreravilar383@gmail.com>
2023-08-10 21:44:39 -0300

    alineacion de slides

--- src/views/ProductosView.vue
+++ src/views/ProductosView.vue
@@ -4,36 +4,70 @@
             <div id="content" class="rounded border border-primary">
                 <h1 class="animate__animated animate__zoomIn"> Productos </h1>
                 <div class="card-group">
-                    <ProductosComponent />
+                    <!-- <ProductosComponent /> -->
                 </div>
                 <div class="row custom-div col-11">
                     <div class="col-4 d-flex flex-column">
                         <h2 class="text-center">COMPUTACIÓN</h2>
-                        <div class="d-flex justify-content-between">
-                        <div class="col-5 me-3 bg-light">
-                            <div>
-                                <p>En Stock</p>
+                        <div class="d-flex justify-content-stretch">
+                            <div class="col-5 me-3 bg-light">
+                                <div>
+                                    <p>En Stock</p>
+                                </div>
+                                <div>
+                                    <img src="./img/AllinOneDell.png" alt="imagen">
+                                    <p>Pc de escritorio All-in-One AIO De 21,5 pulgadas Dell Optiplex 3280 Windows 11 Home
+                                        Intel
+                                        Core i5 / 4 GB RAM / 1 TB</p>
+                                </div>
+                                <div>
+                                    <p>$349.999</p>
+                                </div>
+                                <button>Comprar</button>
                             </div>
-                            <div>
-                                <img src="./img/AllinOneDell.png" alt="imagen">
-                                <p>Pc de escritorio All-in-One AIO De 21,5 pulgadas Dell Optiplex 3280 Windows 11 Home Intel
-                                    Core i5 / 4 GB RAM / 1 TB</p>
+                            <div class="col-5 bg-light">
                             </div>
-                            <div>
-                                <p>$249.999</p>
-                            </div>
-                            <button>Comprar</button>
-                        </div>
-                        <div class="col-5 bg-light">
-                        </div>s
                         </div>
                     </div>
                     <div class="col-4 d-flex flex-column">
                         <h2 class="text-center">CELULARES</h2>
-                        
+                        <div class="d-flex justify-content-stretch">
+                            <div class="col-5 me-3 bg-light">
+                                <div>
+                                    <p>En Stock</p>
+                                </div>
+                                <div>
+                                    <img src="./img/samsung-s21.jpg" alt="imagen">
+                                    <p>Smartphone Samsung Galaxy S21 con pantalla AMOLED de 6.2 pulgadas y cámara de MP.</p>
+                                </div>
+                                <div>
+                                    <p>$249.999</p>
+                                </div>
+                                <button>Comprar</button>
+                            </div>
+                            <div class="col-5 bg-light">
+                            </div>
+                        </div>
                     </div>
                     <div class="col-4 d-flex flex-column">
                         <h2 class="text-center">ACCESORIOS</h2>
+                        <div class="d-flex justify-content-stretch">
+                            <div class="col-5 me-3 bg-light">
+                                <div>
+                                    <p>En Stock</p>
+                                </div>
+                                <div>
+                                    <img src="./img/soporte-celular.jpg" alt="imagen">
+                                    <p>Soporte para celular brazo flexible con pinza ajustable</p>
+                                </div>
+                                <div>
+                                    <p>$795.000</p>
+                                </div>
+                                <button>Comprar</button>
+                            </div>
+                            <div class="col-5 bg-light">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -41,7 +75,7 @@
     </div>
 </template>
 
-<script>
+<!-- <script>
 import ProductosComponent from "../components/ProductosC.vue";
 export default {
     name: "ProductoComponent",
@@ -49,13 +83,12 @@ export default {
         ProductosComponent
     },
 }
-</script>
+</script> -->
 
 <style>
-
 #content {
     text-align: center;
-   
+
 }
 
 .row {
@@ -74,32 +107,30 @@ body {
 .custom-div {
     background: #f3ecbe;
     display: flex;
-    justify-content: space-evenly;
-    margin-left: 60px;
+    justify-content: stretch;
+    margin-left: 70px;
     margin: 0;
     width: 100%;
 }
 
 .custom-div>.col-4 {
     background: #f2eaa0;
-    height: 97%;
     border: 1px solid black;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: stretch;
     align-items: center;
 }
 
-.custom-div>.col-4>.col-5 {
+.custom-div>.col-4>.col-4 {
     background: white;
 }
 
-.col-5>.me-3>.bg-light{
+.col-4>.bg-light {
     height: 100px;
-    width: 80rem;
-    }
+    width: 70rem;
+}
 
 img {
     margin-left: 0;
 }
-
 </style>
