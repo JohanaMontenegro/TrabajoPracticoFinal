97f41b4fca33d935c24563fb3ee4055d6fc90b5b
JohanaMontenegro <yoh.teencontre@gmail.com>
2023-08-17 20:01:43 -0300

    cambios recientes

--- src/App.vue
+++ src/App.vue
@@ -57,29 +57,7 @@
       <div class="sidebar">
         <div>
           <section class="home-section">
-<<<<<<< Updated upstream
-
-            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling"
-              aria-controls="offcanvasScrolling">Enable body scrolling</button>
-
-            <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
-              id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-              <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-              <div class="offcanvas-body">
-                <p>Try scrolling the rest of the page to see this option in action.</p>
-              </div>
-            </div>
-
-            <div class="home-content">
-              <box-icon name='menu'></box-icon>
-              <span class="text">Menú</span>
-            </div>
-=======
             <i class="name"> <img src="./assets/logoAzul.jpg" alt="logo" /></i>
->>>>>>> Stashed changes
           </section>
         </div>
         <div class="options">
