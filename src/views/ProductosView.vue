bd30e6c1aa1185483de7cb179bae1675901fe21d
AidaHerreravilar <aidaherreravilar383@gmail.com>
2023-08-24 22:14:23 -0300

    botones e indicadores

--- src/views/ProductosView.vue
+++ src/views/ProductosView.vue
@@ -11,7 +11,7 @@
                         <h2 class="text-center">COMPUTACIÓN</h2>
                         <div class="d-flex justify-content-stretch">
                             <div class="col-5 me-3 bg-light">
-                                <div>
+                                <div class="stock">
                                     <p>En Stock</p>
                                 </div>
                                 <div>
@@ -20,7 +20,7 @@
                                         Intel
                                         Core i5 / 4 GB RAM / 1 TB</p>
                                 </div>
-                                <div>
+                                <div class="precio">
                                     <p>$349.999</p>
                                 </div>
                                 <button>Comprar</button>
@@ -33,14 +33,14 @@
                         <h2 class="text-center">CELULARES</h2>
                         <div class="d-flex justify-content-stretch">
                             <div class="col-5 me-3 bg-light">
-                                <div>
+                                <div class="stock">
                                     <p>En Stock</p>
                                 </div>
                                 <div>
                                     <img src="./img/samsung-s21.jpg" alt="imagen">
                                     <p>Smartphone Samsung Galaxy S21 con pantalla AMOLED de 6.2 pulgadas y cámara de MP.</p>
                                 </div>
-                                <div>
+                                <div class="precio">
                                     <p>$249.999</p>
                                 </div>
                                 <button>Comprar</button>
@@ -53,26 +53,31 @@
                         <h2 class="text-center">ACCESORIOS</h2>
                         <div class="d-flex justify-content-stretch">
                             <div class="col-5 me-3 bg-light">
-                                <div>
+                                <div class="stock">
                                     <p>En Stock</p>
                                 </div>
                                 <div>
                                     <img src="./img/soporte-celular.jpg" alt="imagen">
                                     <p>Soporte para celular brazo flexible con pinza ajustable</p>
                                 </div>
-                                <div>
+                                <div class="precio">
                                     <p>$795.000</p>
                                 </div>
-                                <button>Comprar</button>
+                                <div class="comprar">
+                                    <button>Comprar</button>
+                                </div>
                             </div>
                             <div class="col-5 bg-light">
                             </div>
                         </div>
                     </div>
                 </div>
+                <div classs="actualizar">
+                    <box-icon name='revision'>Actualizar</box-icon>
             </div>
         </div>
     </div>
+</div>
 </template>
 
 <!-- <script>
@@ -87,8 +92,44 @@ export default {
 
 <style>
 #content {
-    text-align: center;
+    text-overflow: initial;
 
+}
+
+.stock {
+    background-color: #f1d209;
+    border-radius: 25px;
+
+}
+
+.precio {
+    background-color: #98e9c3;
+    border-radius: 25px;
+    border-color: #21e68a;
+    border: solid 1px;
+    font-size: 12px;
+    margin-bottom: 0;
+
+}
+
+.comprar{
+    margin-top: 5px;
+}
+
+button {
+    background-color: #98e9c3;
+    border-radius: 25px;
+    border-color: #21e68a;
+    border: solid 1px;
+    font-size: 12px;
+
+}
+
+.actualizar{
+    display: flex;
+    align-items: center;
+    align-content: center;
+  
 }
 
 .row {
@@ -97,7 +138,7 @@ export default {
 }
 
 body {
-    font-family: system-ui;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
     color: rgb(0, 0, 0);
     text-align: center;
     font-weight: bold;
@@ -105,13 +146,15 @@ body {
 }
 
 .custom-div {
-    
+
     background: #f3ecbe;
     display: flex;
     justify-content: stretch;
     margin-left: 70px;
     margin: 0;
     width: 100%;
+    border-radius: solid #f2d40f 20px;
+
 }
 
 .custom-div>.col-4 {
@@ -121,10 +164,13 @@ body {
     justify-content: stretch;
     align-items: center;
 
+
 }
 
 .custom-div>.col-4>.col-4 {
     background: white;
+    border-radius: 25px;
+    margin-left: 120px;
 }
 
 .col-4>.bg-light {
@@ -134,5 +180,12 @@ body {
 
 img {
     margin-left: 0;
+}
+
+.col-5 {
+
+    background-color: #f1d209;
+    border-radius: solid 25px;
+
 }
 </style>
