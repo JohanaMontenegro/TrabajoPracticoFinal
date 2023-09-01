09904eae3b04dc7856c040ed37e290d189c97650
yerman <germanwibaux@gmail.com>
2023-08-31 21:27:23 -0300

    Cards en productos

--- src/views/ProductosView.vue
+++ src/views/ProductosView.vue
@@ -9,21 +9,26 @@
                 <div class="row custom-div col-11">
                     <div class="col-4 d-flex flex-column">
                         <h2 class="text-center">COMPUTACIÓN</h2>
-                        <div class="d-flex justify-content-stretch">
-                            <div class="col-5 me-3 bg-light">
-                                <div class="stock">
-                                    <p>En Stock</p>
+                        <div class="d-flex justify-content-stretch contenedor-cards">
+                            <div class="col-5 me-3 bg-light inside-contenedor">
+                                <div class="header">
+                                    <div class="stock">
+                                        <p>En Stock</p>
+                                    </div>
+                                    <div>
+                                        <img src="./img/AllinOneDell.png" alt="imagen">
+                                        <p>Pc de escritorio All-in-One AIO De 21,5 pulgadas Dell Optiplex 3280 Windows 11
+                                            Home
+                                            Intel
+                                            Core i5 / 4 GB RAM / 1 TB</p>
+                                    </div>
                                 </div>
-                                <div>
-                                    <img src="./img/AllinOneDell.png" alt="imagen">
-                                    <p>Pc de escritorio All-in-One AIO De 21,5 pulgadas Dell Optiplex 3280 Windows 11 Home
-                                        Intel
-                                        Core i5 / 4 GB RAM / 1 TB</p>
+                                <div class="footer">
+                                    <div class="precio">
+                                        <p>$349.999</p>
+                                    </div>
+                                    <button class="boton-comprar">Comprar</button>
                                 </div>
-                                <div class="precio">
-                                    <p>$349.999</p>
-                                </div>
-                                <button>Comprar</button>
                             </div>
                             <div class="col-5 bg-light">
                             </div>
@@ -31,19 +36,24 @@
                     </div>
                     <div class="col-4 d-flex flex-column">
                         <h2 class="text-center">CELULARES</h2>
-                        <div class="d-flex justify-content-stretch">
-                            <div class="col-5 me-3 bg-light">
-                                <div class="stock">
-                                    <p>En Stock</p>
+                        <div class="d-flex justify-content-stretch contenedor-cards ">
+                            <div class="col-5 me-3 bg-light inside-contenedor">
+                                <div class="header">
+                                    <div class="stock">
+                                        <p>En Stock</p>
+                                    </div>
+                                    <div>
+                                        <img src="./img/samsung-s21.jpg" alt="imagen">
+                                        <p>Smartphone Samsung Galaxy S21 con pantalla AMOLED de 6.2 pulgadas y cámara de MP.
+                                        </p>
+                                    </div>
                                 </div>
-                                <div>
-                                    <img src="./img/samsung-s21.jpg" alt="imagen">
-                                    <p>Smartphone Samsung Galaxy S21 con pantalla AMOLED de 6.2 pulgadas y cámara de MP.</p>
+                                <div class="footer">
+                                    <div class="precio">
+                                        <p>$249.999</p>
+                                    </div>
+                                    <button class="boton-comprar">Comprar</button>
                                 </div>
-                                <div class="precio">
-                                    <p>$249.999</p>
-                                </div>
-                                <button>Comprar</button>
                             </div>
                             <div class="col-5 bg-light">
                             </div>
@@ -51,20 +61,22 @@
                     </div>
                     <div class="col-4 d-flex flex-column">
                         <h2 class="text-center">ACCESORIOS</h2>
-                        <div class="d-flex justify-content-stretch">
-                            <div class="col-5 me-3 bg-light">
-                                <div class="stock">
-                                    <p>En Stock</p>
+                        <div class="d-flex justify-content-stretch contenedor-cards">
+                            <div class="col-5 me-3 bg-light inside-contenedor">
+                                <div class="header">
+                                    <div class="stock">
+                                        <p>En Stock</p>
+                                    </div>
+                                    <div>
+                                        <img src="./img/soporte-celular.jpg" alt="imagen">
+                                        <p>Soporte para celular brazo flexible con pinza ajustable</p>
+                                    </div>
                                 </div>
-                                <div>
-                                    <img src="./img/soporte-celular.jpg" alt="imagen">
-                                    <p>Soporte para celular brazo flexible con pinza ajustable</p>
-                                </div>
-                                <div class="precio">
-                                    <p>$795.000</p>
-                                </div>
-                                <div class="comprar">
-                                    <button>Comprar</button>
+                                <div class="footer">
+                                    <div class="precio">
+                                        <p>$795.000</p>
+                                    </div>
+                                    <button class="boton-comprar">Comprar</button>
                                 </div>
                             </div>
                             <div class="col-5 bg-light">
@@ -74,10 +86,10 @@
                 </div>
                 <div classs="actualizar">
                     <box-icon name='revision'>Actualizar</box-icon>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <!-- <script>
@@ -112,9 +124,10 @@ export default {
 
 }
 
-.comprar{
-    margin-top: 5px;
+.precio>p {
+    margin-bottom: 0;
 }
+
 
 button {
     background-color: #98e9c3;
@@ -125,11 +138,11 @@ button {
 
 }
 
-.actualizar{
+.actualizar {
     display: flex;
     align-items: center;
     align-content: center;
-  
+
 }
 
 .row {
@@ -187,5 +200,22 @@ img {
     background-color: #f1d209;
     border-radius: solid 25px;
 
+}
+
+.boton-comprar {
+    font-weight: 700;
+    margin: 1rem 0;
+    font-size: 15px;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+
+.contenedor-cards {
+    height: 89%;
+}
+
+.inside-contenedor {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
 </style>
