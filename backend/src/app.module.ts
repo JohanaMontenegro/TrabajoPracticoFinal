2ad161af8543d291199e77798eef87ded36a3bfd
AidaHerreravilar <aidaherreravilar383@gmail.com>
2023-09-14 23:50:28 -0300

    cambios backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { CompraModule } from './compra/compra.module';
+import { EnvioModule } from './envio/envio.module';
+import { MedioDePagoModule } from './medio_de_pago/medio_de_pago.module';
+import { CarritoModule } from './carrito/carrito.module';
 
 @Module({
-  imports: [],
+  imports: [UsuarioModule, CompraModule, EnvioModule, MedioDePagoModule, CarritoModule],
   controllers: [AppController],
   providers: [AppService],
 })
