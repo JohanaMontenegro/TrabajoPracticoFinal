aefa29ab6c6f486e83aa505140f01ab16dc05c21
AidaHerreravilar <aidaherreravilar383@gmail.com>
2023-11-27 18:42:30 -0300

    carrito de compras

--- src/components/ComprasProductos.vue
+++ src/components/ComprasProductos.vue
@@ -1,6 +1,30 @@
 <template>
- <h3>Carrito de Compras</h3>
- </template>
+  <div class="row">
+    <div class="col-11">
+      <div id="contents" class="rounded border border-primary">
+        <h2>Carrito de Compras</h2>
+        <ComprasProductos />
+        <ul>
+          <li v-for="producto in productos" :key="producto.id">
+            {{ producto.nombre }} - ${{ producto.precio }}
+            <button @click="agregarAlCarrito(producto)">Agregar</button>
+          </li>
+
+          <li v-for="item in carrito" :key="item.Producto.id">
+            {{ item.Producto.nombre }} - Cantidad: {{ item.quantity }} - Total: ${{
+              item.total
+            }}
+            <button @click="eliminarDelCarrito(item.Producto)">
+              Eliminar del Carrito
+            </button>
+            <button @click="aumentarCantidad(item)">+</button>
+            <button @click="disminuirCantidad(item)">-</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
 <script>
 
 export default {
@@ -11,7 +35,7 @@ export default {
   data() {
     return {
       productos: [],
-      actualizar:false,
+      actualizar: false,
     };
   },
   methods: {
@@ -38,3 +62,9 @@ export default {
 };
 
 </script>
+
+<style scoped>
+h2{
+  text-align: center;
+}
+</style>
--- src/views/ComprasProductosView.vue
+++ src/views/ComprasProductosView.vue
@@ -2,41 +2,23 @@
   <div class="row">
     <div class="col-11">
       <div id="contents" class="rounded border border-primary">
-        <h2>Carrito de Compras</h2>
-        <ul>
-          <li v-for="producto in productos" :key="producto.id">
-            {{ producto.nombre }} - ${{ producto.precio }}
-            <button @click="agregarAlCarrito(producto)">Agregar</button>
-          </li>
-
-          <li v-for="item in carrito" :key="item.Producto.id">
-            {{ item.Producto.nombre }} - Cantidad: {{ item.quantity }} - Total: ${{
-              item.total
-            }}
-            <button @click="eliminarDelCarrito(item.Producto)">
-              Eliminar del Carrito
-            </button>
-            <button @click="aumentarCantidad(item)">+</button>
-            <button @click="disminuirCantidad(item)">-</button>
-          </li>
-        </ul>
+        <ComprasProductos />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ComprasProductos from "../components/ComprasProductos.vue"
 export default {
-  data() {
-    return {
-      carrito: [],
-    };
-  },
-  methods: {},
-};
+  name: "ComprasView",
+  components: {
+    ComprasProductos
+  }
+}
 </script>
 <style scoped>
-h2{
+h2 {
   text-align: center;
 }
 </style>
